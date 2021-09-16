70a350067434c00f1497c10719590ff8b3f56e49
Rafet Akkaya <akkayaraf@gmail.com>
2021-09-16 21:47:15 +0300

    first commit

---
+++ src/Firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+const firebaseApp = firebase.initializeApp({
+
+    // apiKey: "",
+    // authDomain: "",
+    // projectId: "",
+    // storageBucket: "",
+    // messagingSenderId: "",
+    // appId: ""
+
+});
+const db = firebaseApp.firestore();
+export default db;
